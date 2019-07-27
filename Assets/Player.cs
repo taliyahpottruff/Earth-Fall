4e376c7358aeed19af6a71172f04f2ac9770f430
Trenton Pottruff <jayitinc@gmail.com>
2019-07-26 23:12:55 -0400

    Better enemy spawns

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using System.Collections;
 
 public class Player : MonoBehaviour
@@ -7,6 +8,7 @@ public class Player : MonoBehaviour
     public Camera mainCamera;
     public GUIText levelDisplay;
     public GUIText scoreDisplay;
+	[SerializeField] private TextMeshProUGUI pausedText;
     public GameObject enemyPrefab;
     public CameraScript cameraScript;
     public GameObject gameOver;
@@ -28,9 +30,12 @@ public class Player : MonoBehaviour
     public GameObject silver;
     public GameObject gold;
     private float startingY;
+	[SerializeField] private int diffModifier = 10000;
+	[SerializeField] private int[] medalGates = new int[3];
+	[SerializeField] [Range(0, 10000)] private int[] difficultyGates;
+	private bool paused;
 
-    void Start()
-    {
+    void Start() {
         startingY = Camera.main.transform.position.y;
 
         StartCoroutine(NextLevel());
@@ -42,17 +47,24 @@ public class Player : MonoBehaviour
         hasGold = bool.Parse(PlayerPrefs.GetString("hasGold", "false"));
     }
 
-    void Update()
-    {
+    void Update() {
         mousePosition = Input.mousePosition;
         mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
 
+		Time.timeScale = (paused) ? 0f : 1f;
+		pausedText.enabled = paused;
+
         Rect screen = new Rect(0, 0, Screen.width, Screen.height);
 
-        if (cameraScript.active & screen.Contains(Input.mousePosition))
-            transform.position = mousePosition;
+		if (cameraScript.active & screen.Contains(Input.mousePosition) && !paused) {
+			transform.position = mousePosition;
+		}
 
-        levelDisplay.text = "Level: " + level;
+		if (score > highscore) {
+			highscore = score;
+		}
+
+        levelDisplay.text = "Highscore: " + highscore;
         scoreDisplay.text = "Score: " + score;
 
         bronze.active = hasBronze;
@@ -68,32 +80,23 @@ public class Player : MonoBehaviour
 
             score = (int)(startingY - Camera.main.transform.position.y) * 2;
 
-            if (score >= 100 & !hasBronze)
-            {
+            if (score >= medalGates[0] & !hasBronze) {
                 hasBronze = true;
                 bronzeAchievement.Play();
                 PlayerPrefs.SetString("hasBronze", hasBronze.ToString());
                 PlayerPrefs.Save();
-            }
-
-            if (level >= 10 & !hasSilver)
-            {
+            } else if (score >= medalGates[1] & !hasSilver) {
                 hasSilver = true;
                 silverAchievement.Play();
                 PlayerPrefs.SetString("hasSilver", hasSilver.ToString());
                 PlayerPrefs.Save();
-            }
-
-            if (level >= 100 & !hasGold)
-            {
+            } else if (score >= medalGates[2] & !hasGold) {
                 hasGold = true;
                 goldAchievement.Play();
                 PlayerPrefs.SetString("hasGold", hasGold.ToString());
                 PlayerPrefs.Save();
             }
-        }
-        else
-        {
+        } else {
             Cursor.visible = true;
             gameOver.active = true;
             gameOverButton.active = true;
@@ -101,8 +104,7 @@ public class Player : MonoBehaviour
             gameOverScores.text = "Highscore - " + highscore + " | Score - " + score;
             resetScores.active = true;
 
-            if (Input.GetKeyDown(KeyCode.R))
-            {
+            if (Input.GetKeyDown(KeyCode.R)) {
                 highscore = 0;
                 hasBronze = false;
                 hasSilver = false;
@@ -118,40 +120,28 @@ public class Player : MonoBehaviour
 
     public void EndGame()
     {
-        if (score > highscore)
-        {
-            highscore = score;
-            PlayerPrefs.SetInt("highscore", highscore);
-            PlayerPrefs.Save();
-        }
+		PlayerPrefs.SetInt("highscore", highscore);
+        PlayerPrefs.Save();
     }
 
-    public void StartCoroutines()
-    {
+    public void StartCoroutines() {
         StartCoroutine(NextLevel());
         StartCoroutine(GenerateEnemies());
     }
 
-    IEnumerator GenerateEnemies()
-    {
-        while (cameraScript.active)
-        {
-            int numberOfEnemies = Random.Range(0, level * 2);
+    IEnumerator GenerateEnemies() {
+        while (cameraScript.active) {
+			float difficulty = (score * score) / diffModifier;
+			int numberOfEnemies = Mathf.RoundToInt(Random.Range(difficulty/2, difficulty));
 
-            for (int i = 1; i <= numberOfEnemies; i++)
-            {
-                int y = Random.Range(0, 100);
-
-                Vector2 screenLocation = new Vector2(Random.Range(0, Screen.width), -y);
+            for (int i = 1; i <= numberOfEnemies; i++) {
+                Vector2 screenLocation = new Vector2(Random.Range(0, Screen.width), 0);
                 Vector2 worldLocation = mainCamera.ScreenToWorldPoint(screenLocation);
-                Enemy enemy = enemyPrefab.GetComponent(typeof(Enemy)) as Enemy;
-                enemy.enemyBehaviour = EnemyBehaviour.None;
-                Instantiate(enemyPrefab, worldLocation, Quaternion.identity);
+				worldLocation.y += Random.Range(-5f, 0f);
 
                 float random = Random.Range(0f, 1f);
 
-                if (level >= 10 & random < (float)((float) level / 100f))
-                {
+                if (score >= difficultyGates[0] & random < (float)((float) level / 100f)) { //Moving Enemy
                     Enemy enemy1 = enemyPrefab.GetComponent(typeof(Enemy)) as Enemy;
                     enemy1.enemyBehaviour = EnemyBehaviour.Up;
                     Instantiate(enemyPrefab, worldLocation, Quaternion.identity);
@@ -163,7 +153,11 @@ public class Player : MonoBehaviour
                     Enemy enemy4 = enemyPrefab.GetComponent(typeof(Enemy)) as Enemy;
                     enemy4.enemyBehaviour = EnemyBehaviour.Right;
                     Instantiate(enemyPrefab, worldLocation, Quaternion.identity);
-                }
+                } else { //Regular Enemy
+					Enemy enemy = enemyPrefab.GetComponent(typeof(Enemy)) as Enemy;
+					enemy.enemyBehaviour = EnemyBehaviour.None;
+					Instantiate(enemyPrefab, worldLocation, Quaternion.identity);
+				}
             }
 
             yield return new WaitForSeconds(1);
